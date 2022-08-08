407176d2891e3487aba09b6ab82244ed0b1424e7
DungGLA <adung.hver23@gmail.com>
2022-08-08 22:35:09 +0700

    implement deploy

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ae47540d7316545bf9f75d97be836c83-872107457.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
